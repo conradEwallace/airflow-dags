8f6999f9b94cc9a9b8384a2138a28410e37471f4
Conrad Wallace <conradewallace@gmail.com>
2024-10-22 20:48:03 +0100

    Initial commit

---
+++ dags/dbt/data_pipeline/tests/fct_orders_date_valid.sql
@@ -0,0 +1,7 @@
+select
+    *
+from
+    {{ref('fct_orders')}}
+where
+    date(order_date) > current_date()
+    or date(order_date) < date('1900-01-01')
---
+++ dags/dbt_dag.py
@@ -0,0 +1,10 @@
+import os
+from datetime import datetime
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name=
+)
